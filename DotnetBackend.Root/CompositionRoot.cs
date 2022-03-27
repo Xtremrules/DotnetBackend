a16efa6115019e36655053560b331079945fecb9
Xtremrules <olelewextremrules@gmail.com>
2022-03-27 17:09:57 +0100

    Completed Root

---
+++ DotnetBackend.Root/CompositionRoot.cs
@@ -0,0 +1,28 @@
+﻿using DotnetBackend.Data;
+using DotnetBackend.Data.Repositories;
+using DotnetBackend.Data.Repositories.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotnetBackend.Root
+{
+    public class CompositionRoot
+    {
+        public CompositionRoot() { }
+
+        public static void InjectDependencies(IServiceCollection services, IConfiguration config)
+        {
+            // inject dependencies here
+            // db context 
+            services.AddDbContext<AppDbContext>(options => options.UseLazyLoadingProxies()
+            .UseSqlServer(config.GetConnectionString("DefaultConnection")).ConfigureWarnings((config) =>
+                config.Ignore(CoreEventId.LazyLoadOnDisposedContextWarning)
+            ));
+
+            // repositories
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
