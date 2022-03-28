1eb9e9c9fbc0042bd4666e17610c64945de925c4
Xtremrules <olelewextremrules@gmail.com>
2022-03-28 14:37:09 +0100

    Added test project

---
+++ DotnetBackend.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotnetBackend.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
